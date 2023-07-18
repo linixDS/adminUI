40f15885e99d9919fd8511c6aedca77c50a387bd
linix <dmarcisz.ds@gmail.com>
2023-07-18 22:09:10 +0200

    0.0.1

--- components/domains-ui-0.0.1.js
+++ components/domains-ui-0.0.1.js
@@ -13,7 +13,13 @@ export default {
 		  SearchDomainText: '',
 		  ErrorMessage: '',
 		  showContent: true,
-		  modalClasses: ['modal','fade']
+          showDomainContent: false,
+          showButtonPrev: false,
+          createdAdminAccount: false,
+          titlePage: 'Domeny zarejestrowane:',
+          
+          domainName: '',
+          domainComment: '',
 	  }
   },
   
@@ -29,38 +35,45 @@ export default {
   },  
   
   methods: {
-	hideContentMain() {
-		this.ShowContent = false;
+    
+    CheckText(){
+        if (this.domainName.length < 5)
+        {
+            this.createdAdminAccount = false;
+        }
+    },
+    
+	AddNewDomain() {
+        this.createdAdminAccount = false;
+        this.domainName = '';
+        this.domainComment = '';
+        
+      
+        this.titlePage = 'Rejestracja nowej domeny:';
+		this.showContent = false;
+        this.showButtonPrev = true;
+        this.showDomainContent = true;
+        
 		console.log('hide');
 	},
 	
-	showContentMain() {
-		this.ShowContent = true;
+    showAdminAccountPanel(){
+        if (this.domainName.length < 5) this.createdAdminAccount = false; 
+      
+        if (this.createdAdminAccount) {
+            this.createdAdminAccount = false;  
+        }
+          else
+            this.createdAdminAccount = true;  
+    },
+    
+	ShowDomains() {
+        this.showButtonPrev = false;
+        this.showDomainContent = false;
+		this.showContent = true;
+        this.titlePage = 'Domeny zarejestrowane:';
 	},	
 
-    toggle() {
-        document.body.className += ' modal-open';
-        let modalClasses = this.modalClasses;
-    
-        if (modalClasses.indexOf('d-block') > -1) 
-		{
-            modalClasses.pop()
-            modalClasses.pop()
-    
-            //hide backdrop
-            let backdrop = document.querySelector('.modal-backdrop')
-            document.body.removeChild(backdrop)
-        }
-			else {
-				modalClasses.push('d-block')
-				modalClasses.push('show')
-    
-				//show backdrop
-				let backdrop = document.createElement('div')
-				backdrop.classList = "modal-backdrop fade show"
-				document.body.appendChild(backdrop)
-			}
-	},
   
 	  GetDomains() {
                     fetch(this.serverurl+'domains.php?token='+this.token)
@@ -84,28 +97,162 @@ export default {
   },
 
   template: `
-  <a href="#reject" role="button" class="btn btn-primary" @click="toggle()">Launch modal</a>
-            <div :class="modalClasses" class="fade" id="reject" role="dialog">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Modal</h4>
-                            <button type="button" class="close" @click="toggle()">&times;</button>
-                        </div>
-                        <div class="modal-body"> ... </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" @click="toggle()">Close</button>
-                        </div>
+    <div style="margin-bottom: 25px;">
+      <div class="row">
+              <div class="col-1" style="width: 50px;" v-if="showButtonPrev">
+                  <button class="btn btn-outline-primary" v-on:click="ShowDomains">
+                    <i class="fas fa-chevron-left"></i>
+                  </button>
+              </div>
+              <div class="col-xl">
+                <h3 class="text-primary">{{ titlePage }}</h3>
+              </div>
+      </div>
+      <hr style="color: blue;">
+   </div>
+
+    <div v-if="showDomainContent">
+          <div class="row g-3 align-items-center" style="margin-bottom: 20px;">
+            <div class="col-2">
+              <label class="col-form-label"><b>Nazwa domeny:</b>  </label>
+            </div>
+            <div class="col-4">
+              <input type="text" class="form-control" v-model="domainName" @input="CheckText">
+            </div>
+          </div>
+          
+          <div class="row g-3 align-items-center" style="margin-bottom: 20px;">
+            <div class="col-2">
+              <label class="col-form-label">Komentarz: </label>
+            </div>
+            <div class="col-4">
+              <input type="text" class="form-control">
+            </div>
+          </div>
+          
+          <h5 class="text-primary">Usługi:</h5>
+          
+          <div class="row g-3" style="margin-bottom: 20px;">
+                <div class="col-4">
+                    <div class="list-group" >
+                          <label class="list-group-item d-flex gap-2">
+                            <input class="form-check-input flex-shrink-0" type="checkbox" value="">
+                            <span>
+                              WorkFlow
+                              <small class="d-block text-body-secondary">Aktywacja platformy WorkFlow</small>
+                            </span>
+                          </label>
+                          <label class="list-group-item d-flex gap-2">
+                            <input class="form-check-input flex-shrink-0" type="checkbox" value="">
+                            <span>
+                              Chat
+                              <small class="d-block text-body-secondary">Aktywacja usługi Chat</small>
+                            </span>
+                          </label>                          
                     </div>
                 </div>
+                 <div class="col-4">
+                      <div class="list-group">
+                          <label class="list-group-item d-flex gap-2">
+                            <input class="form-check-input flex-shrink-0" type="checkbox" value="">
+                            <span>
+                              Online-Files
+                              <small class="d-block text-body-secondary">Aktywacja platformy Online-Files</small>
+                            </span>
+                          </label>
+                        </div>
+                 </div>
+  
+          </div>  
+          
+          <h5 class="text-primary">Funkcje dodatkowe:</h5>
+          <div class="list-group" style="margin-bottom: 20px;">
+                <label class="list-group-item d-flex gap-2">
+                  <input class="form-check-input flex-shrink-0" type="checkbox" value="" v-model="createdAdminAccount" :disabled="domainName.length < 5">
+                  <span>
+                    Utwórz administratora
+                    <small class="d-block text-body-secondary">Utwórz dedykowanego administratora domeny.</small>
+                  </span>
+                </label>  
+        </div>
+        
+        <div v-if="createdAdminAccount" style="margin-bottom: 20px;">
+        
+            <h6 class="text-primary">Konto administratora dedykowanego:</h6>
+            <div class="row g-3 align-items-center" style="margin-bottom: 20px;">
+                <div class="col-2">
+                  <label class="col-form-label">Nazwa użytkownia:</label>
+                </div>
+                <div class="col-4">
+                  <input type="text" class="form-control">
+                </div>
             </div>
-  
-  
+            <div class="row g-3 align-items-center" style="margin-bottom: 20px;">
+                <div class="col-2">
+                  <label class="col-form-label">Adres E-mail:</label>
+                </div>
+                <div class="col-2">
+                  <input type="text" class="form-control">
+                </div>                
+                <div class="col-2">
+                  <input type="text" class="form-control" :value="'@'+domainName" disabled>
+                </div>
+            </div>            
+             <div class="row g-3 align-items-center" style="margin-bottom: 20px;">
+                <div class="col-2">
+                  <label class="col-form-label">Hasło:</label>
+                </div>
+                <div class="col-4">
+                  <input type="password" class="form-control">
+                </div>
+            </div>
+             <div class="row g-3 align-items-center" style="margin-bottom: 20px;">
+                <div class="col-2">
+                  <label class="col-form-label">Powtórz:</label>
+                </div>
+                <div class="col-4">
+                  <input type="password" class="form-control">
+                </div>
+            </div>               
+            
+            <div class="list-group" style="margin-bottom: 20px;">
+                  <label class="list-group-item d-flex gap-2">
+                    <input class="form-check-input flex-shrink-0" type="checkbox" value="">
+                    <span>
+                      WorkFlow
+                      <small class="d-block text-body-secondary">Aktywuj konto administratora na platformie WorkFlow.</small>
+                    </span>
+                  </label>
+                  <label class="list-group-item d-flex gap-2">
+                    <input class="form-check-input flex-shrink-0" type="checkbox" value="">
+                    <span>
+                      RocketChat
+                      <small class="d-block text-body-secondary">Aktywuj konto administratora na platformie RocketChat.</small>
+                    </span>
+                  </label>
+                  <label class="list-group-item d-flex gap-2">
+                    <input class="form-check-input flex-shrink-0" type="checkbox" value="">
+                    <span>
+                      Online-Disk
+                      <small class="d-block text-body-secondary">Aktywuj konto administratora na platformie Online-Disk.</small>
+                    </span>
+                  </label>                     
+          </div>            
+            
+            
+        </div>
+        
+        
+        
+        
+    </div>
+    
+    
 	<div v-if="showContent">
             <div class="row" style="margin-bottom: 10px;">
                 <div  style="width: 200px;">
-                    <button class="btn btn-outline-primary"  v-on:click="hideContentMain">
-                    <i class="fa fa-plus"></i>   Dodaj domen2ę</button>
+                    <button class="btn btn-outline-primary"  v-on:click="AddNewDomain">
+                    <i class="fa fa-plus"></i>   Dodaj domenę</button>
                 </div>
 				<div  style="width: 400px;">
                     <input class="form-control me-2" type="search" placeholder="Szukaj" aria-label="Szukaj" v-model="SearchDomainText">
