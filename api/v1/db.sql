ce733cabe61e95b3ca562334fcbd8af639d8889f
Dariusz Marcisz <dmarcisz.ds@gmail.com>
2023-07-19 15:52:53 +0200

    230718a

--- api/v1/db.sql
+++ api/v1/db.sql
@@ -1,27 +1,25 @@
--- MariaDB dump 10.19-11.0.2-MariaDB, for Linux (x86_64)
---
--- Host: localhost    Database: admin_panel
--- ------------------------------------------------------
--- Server version	11.0.2-MariaDB
+/*
+SQLyog Community v13.1.9 (64 bit)
+MySQL - 10.4.28-MariaDB : Database - admin_panel
+*********************************************************************
+*/
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`admin_panel` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 
---
--- Table structure for table `accounts`
---
+USE `admin_panel`;
+
+/*Table structure for table `accounts` */
 
 DROP TABLE IF EXISTS `accounts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
   `active` varchar(45) NOT NULL DEFAULT '0',
@@ -40,25 +38,14 @@ CREATE TABLE `accounts` (
   UNIQUE KEY `MAIL_IDX` (`email`),
   KEY `DOMAIN_IDX` (`active`,`domain_id`),
   KEY `TOKEN_IDX` (`token`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `accounts`
---
+/*Data for the table `accounts` */
 
-LOCK TABLES `accounts` WRITE;
-/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `admins`
---
+/*Table structure for table `admins` */
 
 DROP TABLE IF EXISTS `admins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `admins` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
@@ -72,83 +59,92 @@ CREATE TABLE `admins` (
   `createdAccountFiles` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `admins`
---
+/*Data for the table `admins` */
 
-LOCK TABLES `admins` WRITE;
-/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES
+insert  into `admins`(`uid`,`username`,`password`,`type`,`name`,`comment`,`createdAccountMail`,`createdAccountGlpi`,`createdAccountChat`,`createdAccountFiles`) values 
 (1,'GlobalAdmin','0ec02c60874b9c472bf80a568fba27d3','global','Administrator',NULL,0,0,0,0),
 (2,'admin@heban.local','0ec02c60874b9c472bf80a568fba27d3','dedicated','Admin heban.local',NULL,1,0,0,0);
-/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `domain_admins`
---
+/*Table structure for table `domain_admins` */
 
 DROP TABLE IF EXISTS `domain_admins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `domain_admins` (
   `domain_id` int(11) NOT NULL,
   `admin_id` int(11) NOT NULL,
   KEY `index1` (`domain_id`,`admin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `domain_admins`
---
+/*Data for the table `domain_admins` */
 
-LOCK TABLES `domain_admins` WRITE;
-/*!40000 ALTER TABLE `domain_admins` DISABLE KEYS */;
-INSERT INTO `domain_admins` VALUES
+insert  into `domain_admins`(`domain_id`,`admin_id`) values 
 (1,2),
 (2,2);
-/*!40000 ALTER TABLE `domain_admins` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `domains`
---
+/*Table structure for table `domain_services` */
+
+DROP TABLE IF EXISTS `domain_services`;
+
+CREATE TABLE `domain_services` (
+  `domain_id` int(10) unsigned NOT NULL,
+  `service_id` int(11) NOT NULL,
+  KEY `DOMAINSC_IDX` (`domain_id`,`service_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+/*Data for the table `domain_services` */
+
+/*Table structure for table `domains` */
 
 DROP TABLE IF EXISTS `domains`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `domains` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
   `domain` varchar(255) NOT NULL,
   `created` datetime NOT NULL,
   `comment` varchar(255) DEFAULT NULL,
+  `limit_admins` int(11) NOT NULL DEFAULT -1,
+  `limit_mails` int(11) NOT NULL DEFAULT -1,
   PRIMARY KEY (`id`,`domain`),
   UNIQUE KEY `DOMAIN_IDX` (`domain`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `domains`
---
+/*Data for the table `domains` */
 
-LOCK TABLES `domains` WRITE;
-/*!40000 ALTER TABLE `domains` DISABLE KEYS */;
-INSERT INTO `domains` VALUES
-(1,'heban.local','2023-07-14 23:00:49',NULL),
-(2,'salonyhoff.pl','2023-07-14 23:01:06',NULL);
-/*!40000 ALTER TABLE `domains` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+insert  into `domains`(`id`,`active`,`domain`,`created`,`comment`,`limit_admins`,`limit_mails`) values 
+(1,1,'heban.local','2023-07-14 23:00:49',NULL,-1,-1),
+(2,1,'salonyhoff.pl','2023-07-14 23:01:06',NULL,-1,-1);
+
+/*Table structure for table `services` */
+
+DROP TABLE IF EXISTS `services`;
+
+CREATE TABLE `services` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `active` tinyint(1) NOT NULL DEFAULT 1,
+  `name` varchar(35) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `description` varchar(120) DEFAULT NULL,
+  `com_checked` tinyint(1) NOT NULL DEFAULT 0,
+  `com_disabled` tinyint(1) NOT NULL DEFAULT 0,
+  `onInstall` text DEFAULT NULL,
+  `onUninstall` text DEFAULT NULL,
+  `onRegisterUser` text DEFAULT NULL,
+  `onDeleteUser` text DEFAULT NULL,
+  `onExec` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `SERVICE_IDX` (`name`,`active`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+/*Data for the table `services` */
+
+insert  into `services`(`id`,`active`,`name`,`url`,`description`,`com_checked`,`com_disabled`,`onInstall`,`onUninstall`,`onRegisterUser`,`onDeleteUser`,`onExec`) values 
+(1,1,'SOGo','http://192.168.0.3/SOGo','Dostęp do poczty internetowej',1,1,NULL,NULL,NULL,NULL,NULL),
+(2,1,'WorkFlow','http://workflow/','ądzanie zasobami,projektami,zadaniami',0,0,NULL,NULL,NULL,NULL,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-07-17 19:52:25
