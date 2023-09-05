6190090d1161676606ad566896b70dbb9c7ca12b
Dariusz Marcisz <dmarcisz.ds@gmail.com>
2023-09-05 10:04:57 +0200

    config

--- api/v1/config/ldap.php
+++ api/v1/config/ldap.php
@@ -1,8 +1,8 @@
 <?php
 
-	define("CONFIG_LDAP_USER", "cn=admin,dc=system,dc=local");
-	define("CONFIG_LDAP_PASS", "Kamasutra");
+	define("CONFIG_LDAP_USER", "cn=admin,dc=system");
+	define("CONFIG_LDAP_PASS", "hoff01HOFF02");
 	define("CONFIG_LDAP_HOST", "localhost");
     define("CONFIG_LDAP_PORT", 389);
-	define("CONFIG_LDAP_DN", "dc=system,dc=local");
+	define("CONFIG_LDAP_DN", "dc=system");
 ?>
